522acde6cb3862c2e2d1e5c559fedb6265088bd8
Cheng Ly <chengsieuly@gmail.com>
2025-04-05 11:30:28 -0700

    Update carousel styles

--- libs/simplic/src/lib/carousel/index.stories.tsx
+++ libs/simplic/src/lib/carousel/index.stories.tsx
@@ -11,6 +11,7 @@ type Story = StoryObj<typeof Carousel>;
 export const Primary: Story = {
   args: {
     title: 'My carousel',
+    description: 'Some description',
     children: ['1', '2', '3', '4'].map((item) => (
       <div key={item} className="border border-black w-96">
         {item}
--- libs/simplic/src/lib/carousel/index.tsx
+++ libs/simplic/src/lib/carousel/index.tsx
@@ -3,20 +3,23 @@ import React, { useId } from 'react';
 interface CarouselProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
   title: string;
+  description?: string;
 }
 
 export const Carousel: React.FC<CarouselProps> = ({
   children,
   title,
+  description,
   ...rest
 }) => {
   const titleId = useId();
 
   return (
-    <div className="relative">
-      <h2 id={titleId} className="font-semibold text-lg text-center mb-12">
+    <div className="relative space-y-2">
+      <h2 id={titleId} className="font-semibold text-3xl">
         {title}
       </h2>
+      {description && <p className="text-sm text-gray-600">{description}</p>}
       <div
         className="flex overflow-x-auto scroll-snap-x snap-mandatory gap-5"
         role="region"
