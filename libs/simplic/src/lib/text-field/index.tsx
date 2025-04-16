2342a3e0b097c12dc28c32ca36500492c0e0fab1
Cheng Ly <chengsieuly@gmail.com>
2025-04-15 18:40:16 -0700

    Support textfield forward ref

--- libs/simplic/src/lib/text-field/index.tsx
+++ libs/simplic/src/lib/text-field/index.tsx
@@ -7,41 +7,38 @@ interface TextFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
   alignment?: 'left' | 'center';
 }
 
-export const TextField: React.FC<TextFieldProps> = ({
-  label,
-  required,
-  hideLabel,
-  alignment = 'center',
-  ...rest
-}) => {
-  const id = useId();
+export const TextField = React.forwardRef<HTMLInputElement, TextFieldProps>(
+  ({ label, required, hideLabel, alignment = 'center', ...rest }, ref) => {
+    const id = useId();
 
-  return (
-    <div className="flex flex-col space-y-2">
-      <label
-        htmlFor={id}
-        className={cn('text-sm text-center', {
-          'sr-only': hideLabel,
-        })}
-      >
-        {label}
-        {required && <span className="text-red-500">*</span>}
-      </label>
-      <input
-        type="text"
-        id={id}
-        required={required}
-        aria-required={required}
-        className={cn(
-          'px-3 py-2 bg-neutral-100 rounded-sm focus:outline-none focus:ring-1 focus:ring-black',
-          {
-            'text-center': alignment === 'center',
-            'text-left': alignment === 'left',
-          }
-        )}
-        aria-label={label}
-        {...rest}
-      />
-    </div>
-  );
-};
+    return (
+      <div className="flex flex-col space-y-2">
+        <label
+          htmlFor={id}
+          className={cn('text-sm text-center', {
+            'sr-only': hideLabel,
+          })}
+        >
+          {label}
+          {required && <span className="text-red-500">*</span>}
+        </label>
+        <input
+          type="text"
+          id={id}
+          required={required}
+          aria-required={required}
+          className={cn(
+            'px-3 py-2 bg-neutral-100 rounded-sm focus:outline-none focus:ring-1 focus:ring-black',
+            {
+              'text-center': alignment === 'center',
+              'text-left': alignment === 'left',
+            }
+          )}
+          aria-label={label}
+          ref={ref}
+          {...rest}
+        />
+      </div>
+    );
+  }
+);
