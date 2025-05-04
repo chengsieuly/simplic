1a13df3499814e3238a405e1a4c61d434cedee57
Cheng Ly <chengsieuly@gmail.com>
2025-05-04 08:58:13 -0700

    Add image removal capabilities

--- libs/simplic/src/lib/drag-and-drop/index.tsx
+++ libs/simplic/src/lib/drag-and-drop/index.tsx
@@ -1,7 +1,16 @@
-import { CloudArrowUpIcon } from '@heroicons/react/24/outline';
-import { XCircleIcon } from '@heroicons/react/24/solid';
+import {
+  ArrowUturnLeftIcon,
+  CloudArrowUpIcon,
+  SparklesIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline';
+import { Config, preload, removeBackground } from '@imgly/background-removal';
 import cn from 'classnames';
 import { MouseEventHandler, useEffect, useId, useRef, useState } from 'react';
+import { IconButton } from '../button';
+import { LoadingAnimation } from '../loading-animation';
+import { toast } from '../toasts';
+import { blobToDataURL } from '../utils';
 import { preventDefaults } from '../utils/prevent-defaults';
 
 type DragAndDropImageShape = 'circle';
@@ -20,9 +29,23 @@ export const DragAndDrop = ({
   const id = useId();
   const dropAreaRef = useRef<HTMLDivElement>(null);
   const [shouldHighlight, setShouldHighlight] = useState(false);
+  const [beforeRemovingBgPreview, setBeforeRemovingBgPreview] = useState<
+    string | ArrayBuffer | null | undefined
+  >();
   const [preview, setPreview] = useState<
     string | ArrayBuffer | null | undefined
   >();
+  const [isRemovingBg, setIsRemovingBg] = useState(false);
+
+  const bgRemovalConfig: Config = {
+    debug: false,
+    rescale: true,
+    device: 'gpu',
+    output: {
+      quality: 0.8,
+      format: 'image/png',
+    },
+  };
 
   const handleAvatarImageSelectionChange: React.ChangeEventHandler<
     HTMLInputElement
@@ -34,6 +57,7 @@ export const DragAndDrop = ({
       reader.onload = function (e) {
         setPreview(e.target?.result);
         onImageSelect(file);
+        setBeforeRemovingBgPreview(null);
       };
       reader.readAsDataURL(file);
     }
@@ -48,6 +72,7 @@ export const DragAndDrop = ({
       reader.onload = function (e) {
         setPreview(e.target?.result);
         onImageSelect(file);
+        setBeforeRemovingBgPreview(null);
       };
       reader.readAsDataURL(file);
     }
@@ -58,6 +83,31 @@ export const DragAndDrop = ({
     e.preventDefault();
     setPreview(null);
     onImageSelect(null);
+    setBeforeRemovingBgPreview(null);
+  };
+
+  const handleRemoveBackground = async () => {
+    if (preview) {
+      setBeforeRemovingBgPreview(preview);
+      setIsRemovingBg(true);
+      try {
+        const imageBlob = await removeBackground(preview, bgRemovalConfig);
+        const dataUrl = await blobToDataURL(imageBlob);
+        setPreview(dataUrl);
+      } catch (error) {
+        console.error('Processing failed:', error);
+        toast.critical(
+          'Sorry, we were unable to remove your background image.'
+        );
+      } finally {
+        setIsRemovingBg(false);
+      }
+    }
+  };
+
+  const handleRevertBgRemoval = () => {
+    setPreview(beforeRemovingBgPreview);
+    setBeforeRemovingBgPreview(null);
   };
 
   const highlight = () => setShouldHighlight(true);
@@ -116,15 +166,29 @@ export const DragAndDrop = ({
     }
   }, [file]);
 
+  // handle bg removal
+  useEffect(() => {
+    const preloadAssets = async () => {
+      try {
+        await preload(bgRemovalConfig);
+        console.log('Asset preloading succeeded');
+      } catch (error) {
+        console.error('Asset preloading failed:', error);
+      }
+    };
+
+    preloadAssets();
+  }, []);
+
   return (
     <div
       ref={dropAreaRef}
-      className="group flex items-center justify-center w-full h-60 bg-gray-50"
+      className="relative group flex items-center justify-center w-full h-60 bg-gray-50"
     >
       <label
         htmlFor={id}
         className={cn(
-          'flex flex-col items-center justify-center w-full h-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer hover:bg-gray-100 p-5',
+          'relative flex flex-col items-center justify-center w-full h-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer hover:bg-gray-100 p-5',
           {
             'bg-gray-100': shouldHighlight,
           }
@@ -132,21 +196,20 @@ export const DragAndDrop = ({
       >
         <div className="flex flex-col items-center justify-center h-full space-y-1">
           {preview ? (
-            <div className="flex items-center justify-center relative w-48 h-48">
+            <div className="relative flex justify-center items-center w-48 h-48">
               <img
                 src={preview as string}
                 alt="image preview"
                 className={cn('h-full object-cover', {
                   'rounded-full w-full': shape === 'circle',
+                  'opacity-30': isRemovingBg,
                 })}
               />
-              <button
-                type="button"
-                onClick={handlRemoveImage}
-                className="absolute top-0 flex h-full w-full justify-center items-center opacity-0 group-hover:opacity-70 transition-opacity"
-              >
-                <XCircleIcon className="w-10 cursor-pointer" />
-              </button>
+              {isRemovingBg && (
+                <div className="absolute">
+                  <LoadingAnimation />
+                </div>
+              )}
             </div>
           ) : (
             <>
@@ -167,6 +230,22 @@ export const DragAndDrop = ({
           accept="image/jpeg, image/png"
         />
       </label>
+      {preview && (
+        <div className="flex flex-col gap-1 absolute top-3 right-3">
+          {!!beforeRemovingBgPreview ? (
+            <IconButton
+              icon={<ArrowUturnLeftIcon />}
+              onClick={handleRevertBgRemoval}
+            />
+          ) : (
+            <IconButton
+              icon={<SparklesIcon />}
+              onClick={handleRemoveBackground}
+            />
+          )}
+          <IconButton icon={<XMarkIcon />} onClick={handlRemoveImage} />
+        </div>
+      )}
     </div>
   );
 };
--- libs/simplic/src/lib/utils/index.ts
+++ libs/simplic/src/lib/utils/index.ts
@@ -1,3 +1,4 @@
+export * from './blob-to-data-url';
 export * from './is-image-valid';
 export * from './is-valid-date';
 export * from './prevent-defaults';
